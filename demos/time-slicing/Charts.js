99de12a46eeafa45d6db31a077d0e160ad06ade4
sw-yx <swyx@swyx.io>
2018-08-08 11:11:02 -0400

    finished timeslicing charts

--- demos/time-slicing/Charts.js
+++ demos/time-slicing/Charts.js
@@ -16,188 +16,28 @@ const colors = ['#fff489', '#fa57c1', '#b166cc', '#7572ff', '#69a6f9'];
 
 export default class Charts extends Component {
   render() {
-    const streamData = this.props.data;
-    const data = streamData // [streamData[0]]
+    const data = this.props.data;
+    // const data = streamData // [streamData[0]]
+    console.log({data})
     const container = document.getElementById("demo")
     if (!container) return <div>Type something</div> // initial load only
-
-    var margin = { top: 10, right: 20, bottom: 30, left: 30 };
-    var width = 565 - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
-
-    // really janky clearance
-    const temp = d3.select(".chart")
-      .selectAll("svg")
-    if (temp._groups[0] && temp._groups[0].length) temp._groups[0]
-      .forEach((d, i) => d.parentNode.removeChild(d))
-    
-    var svg = d3.select('.chart')
-      .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        // .call(responsivefy)
-      .append('g')
-        .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-      
-      var xScale = d3.scaleLinear()
-        .domain([
-          d3.min(data, co => d3.min(co, d => d.x)),
-          d3.max(data, co => d3.max(co, d => d.x))
-        ])
-        .range([0, width]);
-      svg
-        .append('g')
-          .attr('transform', `translate(0, ${height})`)
-          .style('stroke', 'yellow')
-        .call(d3.axisBottom(xScale).ticks(5));
-    
-      var yScale = d3.scaleLinear()
-        .domain([
-          d3.min(data, co => d3.min(co, d => d.y)),
-          d3.max(data, co => d3.max(co, d => d.y))
-        ])
-        .range([height, 0]);
-      svg
-        .append('g')
-        .style('stroke', 'yellow')
-        .call(d3.axisLeft(yScale));
-
-
-      var line = d3.line()
-        .x(d => xScale(d.x))
-        .y(d => yScale(d.y))
-        .curve(d3.curveCatmullRom.alpha(0.5));
-    
-      svg
-        .selectAll('.line')
-        .data(data)
-        .enter()
-        .append('path')
-        .attr('class', 'line')
-        .attr('d', d => line(d))
-        // .style('stroke', (d, i) => ['#FF9900', '#3369E8'][i])
-        .style('stroke', (d, i) => colors[i])
-        .style('stroke-width', 2)
-        .style('fill', 'none');
-    // var scale = d3.scaleLinear()
-    //   .domain([0, 1000])
-    //   .range([0, 1000])
-    // const path = d3.select(".chart")
-    //   .selectAll("div")
-    //   .data(streamData[0].map(x => x.y))
-    // // enter
-    // path
-    //     .enter()
-    //     .append("div")
-    //     .style("background", "rebeccapurple")
-    //     .style("width", function(d) { return scale(d) + "px"; })
-    //     .text(function(d) { return d; });
+    bottomChart(data)
+    leftChart(data)
+    rightChart(data)
     return (
       <div>
-        <div style={{display: 'flex'}}>
-          <div className="chart"></div>
-          {/* <VictoryChart
-            theme={VictoryTheme.material}
-            width={400}
-            height={400}
-            style={{
-              parent: {
-                backgroundColor: '#222',
-              },
-            }}>
-            <VictoryAxis
-              style={{
-                axis: {stroke: 'white'},
-                tickLabels: {fill: 'white'},
-              }}
-            />
-            <VictoryAxis
-              style={{
-                axis: {stroke: 'white'},
-                tickLabels: {fill: 'white'},
-              }}
-              dependentAxis
-            />
-            <VictoryScatter
-              data={streamData[0]}
-              size={6}
-              style={{
-                data: {
-                  fill: d => colors[d.x % 5],
-                },
-              }}
-            />
-          </VictoryChart>
-
-          <VictoryChart
-            theme={VictoryTheme.material}
-            width={400}
-            height={400}
-            style={{
-              parent: {
-                backgroundColor: '#222',
-              },
-            }}
-            domainPadding={[20, 20]}>
-            <VictoryAxis
-              style={{
-                axis: {stroke: 'white'},
-                tickLabels: {fill: 'white'},
-              }}
-            />
-            <VictoryAxis
-              style={{
-                axis: {stroke: 'white'},
-                tickLabels: {fill: 'white'},
-              }}
-              dependentAxis
-            />
-            <VictoryBar
-              data={streamData[0]}
-              style={{
-                data: {
-                  fill: d => colors[d.x % 5],
-                  stroke: 'none',
-                  padding: 5,
-                },
-              }}
-            />
-          </VictoryChart> */}
+        <h2 style={{textAlign: 'center'}}>number of datapoints: {data[0].length * 5}</h2>
+        <div style={{display: 'flex', paddingBottom: 20}}>
+          <div className="leftchart"></div>
+          <div className="rightchart"></div>
         </div>
         <div
           style={{
             display: 'flex',
             position: 'relative',
-            top: '-50px',
+            // top: '-50px',
           }}>
-          {/* <VictoryChart
-            theme={VictoryTheme.material}
-            width={800}
-            height={350}
-            style={{
-              parent: {
-                backgroundColor: '#222',
-              },
-            }}>
-            <VictoryAxis
-              style={{
-                axis: {stroke: 'white'},
-                tickLabels: {fill: 'white'},
-              }}
-            />
-            <VictoryAxis
-              style={{
-                axis: {stroke: 'white'},
-                tickLabels: {fill: 'white'},
-              }}
-              dependentAxis
-            />
-            <VictoryStack>
-              {streamData.map((data, i) => (
-                <VictoryArea key={i} data={data} colorScale={colors} />
-              ))}
-            </VictoryStack>
-          </VictoryChart> */}
+          <div className="bottomchart"></div>
         </div>
       </div>
     );
@@ -231,4 +71,198 @@ function responsivefy(svg) {
       svg.attr("width", targetWidth);
       svg.attr("height", Math.round(targetWidth / aspect));
   }
+}
+
+function bottomChart(data) {
+
+  var margin = { top: 10, right: 20, bottom: 30, left: 30 };
+  var width = 565 * 2 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
+
+  // really janky clearance
+  const temp = d3.select(".bottomchart")
+    .selectAll("svg")
+  if (temp._groups[0] && temp._groups[0].length) temp._groups[0]
+    .forEach((d, i) => d.parentNode.removeChild(d))
+  
+  var svg = d3.select('.bottomchart')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      // .call(responsivefy)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    
+    var xScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, co => d3.min(co, d => d.x)),
+        d3.max(data, co => d3.max(co, d => d.x))
+      ])
+      .range([0, width]);
+    svg
+      .append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .style('stroke', 'yellow')
+      .call(d3.axisBottom(xScale).ticks(5));
+  
+    var yScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, co => d3.min(co, d => d.y)),
+        d3.max(data, co => d3.max(co, d => d.y))
+      ])
+      .range([height, 0]);
+    svg
+      .append('g')
+      .style('stroke', 'yellow')
+      .call(d3.axisLeft(yScale));
+
+    var area = d3.area()
+      .x(d => xScale(d.x))
+      .y0(yScale(yScale.domain()[0]))
+      .y1(d => yScale(d.y))
+      .curve(d3.curveCatmullRom.alpha(0.5));
+  
+    svg
+      .selectAll('.area')
+      .data(data)
+      .enter()
+      .append('path')
+      .attr('class', 'area')
+      .attr('d', d => area(d))
+      .style('stroke', (d, i) => colors[i])
+      .style('stroke-width', 2)
+      .style('fill', (d, i) => colors[i])
+      .style('fill-opacity', 0.5)
+}
+
+
+
+function rightChart(data) {
+
+  var margin = { top: 10, right: 20, bottom: 30, left: 30 };
+  var width = 565 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
+
+  // really janky clearance
+  const temp = d3.select(".rightchart")
+    .selectAll("svg")
+  if (temp._groups[0] && temp._groups[0].length) temp._groups[0]
+    .forEach((d, i) => d.parentNode.removeChild(d))
+  
+  var svg = d3.select('.rightchart')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      // .call(responsivefy)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    
+    var xScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, co => d3.min(co, d => d.x)),
+        d3.max(data, co => d3.max(co, d => d.x))
+      ])
+      .range([0, width]);
+    svg
+      .append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .style('stroke', 'yellow')
+      .call(d3.axisBottom(xScale).ticks(5));
+  
+    var yScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, co => d3.min(co, d => d.y)),
+        d3.max(data, co => d3.max(co, d => d.y))
+      ])
+      .range([height, 0]);
+    svg
+      .append('g')
+      .style('stroke', 'yellow')
+      .call(d3.axisLeft(yScale));
+
+
+    var line = d3.line()
+      .x(d => xScale(d.x))
+      .y(d => yScale(d.y))
+      .curve(d3.curveCatmullRom.alpha(0.5));
+  
+    svg
+      .selectAll('.line')
+      .data(data)
+      .enter()
+      .append('path')
+      .attr('class', 'line')
+      .attr('d', d => line(d))
+      // .style('stroke', (d, i) => ['#FF9900', '#3369E8'][i])
+      .style('stroke', (d, i) => colors[i])
+      .style('stroke-width', 2)
+      .style('fill', 'none');
+}
+
+
+function leftChart(data) {
+
+  var margin = { top: 10, right: 20, bottom: 30, left: 30 };
+  var width = 565 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
+
+  // really janky clearance
+  const temp = d3.select(".leftchart")
+    .selectAll("svg")
+  if (temp._groups[0] && temp._groups[0].length) temp._groups[0]
+    .forEach((d, i) => d.parentNode.removeChild(d))
+  
+  var svg = d3.select('.leftchart')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      // .call(responsivefy)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    
+    var xScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, co => d3.min(co, d => d.x)),
+        d3.max(data, co => d3.max(co, d => d.x))
+      ])
+      .range([0, width]);
+    svg
+      .append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .style('stroke', 'yellow')
+      .call(d3.axisBottom(xScale).ticks(5));
+  
+    var yScale = d3.scaleLinear()
+      .domain([
+        d3.min(data, co => d3.min(co, d => d.y)),
+        d3.max(data, co => d3.max(co, d => d.y))
+      ])
+      .range([height, 0]);
+    svg
+      .append('g')
+      .style('stroke', 'yellow')
+      .call(d3.axisLeft(yScale));
+
+    var circlesArr = svg
+      .selectAll('.ball')
+      .data(data)
+      .enter()
+      .append('g')
+      .each(function(d, i) {
+        var node = d3.select(this).selectAll('g')
+          .data(d)
+          .enter()
+          .append('g')
+          .attr('class', 'ball')
+          .attr('transform', d => {
+            return `translate(${xScale(d.x)}, ${yScale(d.y)})`;
+          });
+        node
+          .append('circle')
+          .attr('cx', 0)
+          .attr('cy', 0)
+          .attr('r', d => 5)
+          .style('fill-opacity', 0.5)
+          .style('fill', colors[i]);
+      })
 }
